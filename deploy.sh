1c8d4f73385d882dc1ecdf712fad7716d103e35e
Alejandro Navarro <alejandropvp13@gmail.com>
2023-04-07 09:19:43 +0200

    Pruebas deploy

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:alenavarroxp/alenavarroxp.github.io.git main
+git push -f git@github.com:alenavarroxp/alenavarroxp.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:alenavarroxp/alenavarro.github.io.git main:gh-pages
