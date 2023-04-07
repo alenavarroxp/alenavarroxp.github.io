7bfc82d9b1641b732325672e9187bb24756fc598
Alejandro Navarro <alejandropvp13@gmail.com>
2023-04-08 00:58:00 +0200

    Cambios relativos a Educacion y principio de Timeline

--- src/components/Educacion/Educacion.tsx
+++ src/components/Educacion/Educacion.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import AnimatedLine from "../AnimatedComponents/AnimatedLine";
+import Idiomas from "./Idiomas/Idiomas";
+import Timeline from "./Timeline/MyTimeline"
 function Educacion() {
   return (
-    <div className='lg:ml-24 md:ml-24 text-4xl  font-bold w-fit text-white flex-row mt-14  ml-12 '>Educacion</div>
-  ) 
+    <div className="lg:ml-24 md:ml-24 text-4xl font-bold w-fit text-white flex-row mt-14  ml-12 ">
+      <div className="w-fit">
+        Educacion
+        <AnimatedLine />
+      </div>
+      <Timeline/>
+      <Idiomas />
+    </div>
+  );
 }
 
-export default Educacion
+export default Educacion;
---
+++ src/components/Educacion/Idiomas/Idiomas.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AnimatedLine from "../../AnimatedComponents/AnimatedLine";
+
+function Idiomas() {
+  return (
+    <div className="w-fit">
+      Idiomas
+      <AnimatedLine/>
+    </div>
+  );
+}
+
+export default Idiomas;
