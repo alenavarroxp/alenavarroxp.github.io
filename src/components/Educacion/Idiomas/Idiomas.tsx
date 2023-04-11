e5543fb00276f1e78e92759b35da081c961b3442
Alejandro Navarro <alejandropvp13@gmail.com>
2023-04-11 11:54:42 +0200

    Cambios relativos al flip de las tarjetas de idiomas

--- src/components/Educacion/Idiomas/Idiomas.tsx
+++ src/components/Educacion/Idiomas/Idiomas.tsx
@@ -5,24 +5,30 @@ import English from "../../../images/reino-unido.jpg";
 import { motion } from "framer-motion";
 
 const idiomas = [
-  { id: 0, idioma: "Español", certificacion: "Nativo", image: Spain,flip:false },
+  {
+    id: 0,
+    idioma: "Español",
+    certificacion: "Nativo",
+    image: Spain,
+    flip: false,
+  },
   {
     id: 1,
     idioma: "Inglés",
     certificacion: "B1 sin certificar",
     image: English,
-    flip:false
+    flip: false,
   },
 ];
 
 function Idiomas() {
   const [flippedIds, setFlippedIds] = useState<any>({});
 
-  function handleFlip(id:any) {
-    setFlippedIds((prevIds:any) => {
+  function handleFlip(id: any) {
+    setFlippedIds((prevIds: any) => {
       return {
         ...prevIds,
-        [id]: !prevIds[id]
+        [id]: !prevIds[id],
       };
     });
   }
@@ -33,57 +39,58 @@ function Idiomas() {
         <AnimatedLine />
       </div>
       <div className="flex flex-row">
-        {idiomas.map((idioma,id) => (
-          <div className="">
-           
-           <motion.div
+        {idiomas.map((idioma, id) => (
+          <div className="relative">
+            <motion.div
               key={idioma.id}
               animate={
                 flippedIds[idioma.id]
-                  ? { rotateY: 180, opacity: 0 }
+                  ? { rotateY: 180, display:'none' }
                   : { rotateY: 0, opacity: 1 }
               }
-            className="card w-80 h-fit mr-24 shadow-2xl"
-            style={{ borderRadius: "3.7rem" }}
-          >
-            <img
-              src={idioma.image}
-              alt=""
-              className=""
+              transition={{ type: "spring", stiffness: 80 }}
+              className="card w-80 h-fit mr-24 shadow-2xl"
               style={{ borderRadius: "3.7rem" }}
-            />
-            <p
-              className={`absolute text-xl top-5 transform ${
-                idioma.idioma === "Inglés"
-                  ? "translate-x-6 top-24"
-                  : "translate-x-0"
-              } ml-8`}
-              style={{
-                textShadow: "3px 3px 3px rgba(0, 0, 0, 0.7)",
-                color: "#FFFFFF",
-              }}
             >
-              {idioma.idioma} - {idioma.certificacion}
-            </p>
-            <button
-              type="button"
-              className="text-gray-900 absolute bg-white border top-44 right-3 border-gray-300 focus:outline-none hover:bg-gray-200 focus:ring-2 focus:ring-[#0072F5] font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2"
-              onClick={() => handleFlip(idioma.id)}
-              style={{ boxShadow: "3px 3px 3px rgba(0, 0, 0, 0.5)" }}
-            >
-              <div>
-                <p className="text-[#0072F5]">INFO</p>
-              </div>
-            </button>
+              <img
+                src={idioma.image}
+                alt=""
+                className=""
+                style={{ borderRadius: "3.7rem" }}
+              />
+              <p
+                className={`absolute text-xl transform ${
+                  idioma.idioma === "Inglés"
+                    ? "translate-x-6 top-24"
+                    : "translate-x-0 top-5"
+                } ml-8`}
+                style={{
+                  textShadow: "3px 3px 3px rgba(0, 0, 0, 0.7)",
+                  color: "#FFFFFF",
+                }}
+              >
+                {idioma.idioma} - {idioma.certificacion}
+              </p>
+              <button
+                type="button"
+                className="text-gray-900 absolute bg-white border top-44 right-3 border-gray-300 focus:outline-none hover:bg-gray-200 focus:ring-2 focus:ring-[#0072F5] font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2"
+                onClick={() => handleFlip(idioma.id)}
+                style={{ boxShadow: "3px 3px 3px rgba(0, 0, 0, 0.5)" }}
+              >
+                <div>
+                  <p className="text-[#0072F5]">INFO</p>
+                </div>
+              </button>
             </motion.div>
             <motion.div
               key={idioma.id}
-              initial={{ rotate:0,opacity:0 }}
+              initial={{ rotate: 0, opacity: 0 }}
               animate={
                 !flippedIds[idioma.id]
-                  ? { rotateY: 180, opacity: 0 }
+                  ? { rotateY: 180, display:'none'}
                   : { rotateY: 0, opacity: 1 }
               }
+              transition={{ type: "spring", stiffness: 80 }}
               className="card relative w-80 h-fit mr-24 shadow-2xl bg-white"
               style={{ borderRadius: "3.7rem" }}
             >
@@ -111,7 +118,7 @@ function Idiomas() {
                   <p className="text-[#0072F5]">INFO</p>
                 </div>
               </button>
-          </motion.div>
+            </motion.div>
           </div>
         ))}
       </div>
